e2bfa8e219c65cf3a6a86274575d1a5fe3cdb2cb
Chigoziri123 <anuna.s@bullseyemanpower.com>
2023-07-21 11:22:38 +0100

    Added a step to install the dependencies

---
+++ core/tests.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from rest_framework.test import APIClient, APITestCase
+from django.urls import reverse
+
+# Create your tests here.
+
+class SimpleTest(APITestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.path = reverse('home')
+
+    def test_true(self):
+        self.assertEqual(1, 1)
+
+    def test_response(self):
+        res = self.client.post(self.path)
+        self.assertEqual(200, 200)
+        
---
+++ core/views.py
@@ -0,0 +1,7 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+# Create your views here.
+
+def index_view(request):
+    return Response({'status':status.HTTP_200_OK})

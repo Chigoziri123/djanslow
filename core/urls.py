58402782897ce0617cc3870c40658bb6f87b2778
Chigoziri123 <anuna.s@bullseyemanpower.com>
2023-07-21 15:03:30 +0100

    Made one modification

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from core.views import index_view, AddCustomerView
 
 urlpatterns=[
     path('', index_view, name='home'),
-    path('add-customer', AddCustomerView.as_view(), name='add')
+    path('add_customer', AddCustomerView.as_view(), name='add')
 ]
